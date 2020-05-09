e1ebc1315e4a54d01ef42504ed45b4274ce48df0
Bhpsngum <53442511+Bhpsngum@users.noreply.github.com>
2020-05-09 09:56:40 +0700

    Update init.js

--- init.js
+++ init.js
@@ -1,4 +1,4 @@
-$.ajax("https://raw.githubusercontent.com/Bhpsngum/img-src/master/config.js")
+$.ajax("https://raw.githubusercontent.com/dpleshkov/starblast-modding-plus/master/config.js")
 .done(function (data) {
   eval(data);
 })
